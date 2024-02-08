2777609ae2eb25827c1753fc244044a85ff82149
Ole <108256966+OleMarin@users.noreply.github.com>
2024-02-08 14:03:19 +0100

    OnGet Method implemented

--- OlMaPixelBoard/Pages/Index.cshtml.cs
+++ OlMaPixelBoard/Pages/Index.cshtml.cs
@@ -1,12 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http;
 
 namespace OlMaPixelBoard.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        static readonly HttpClient client = new HttpClient();
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -14,7 +15,19 @@ namespace OlMaPixelBoard.Pages
 
         public void OnGet()
         {
+            HttpResponseMessage response = client.GetAsync("https://edu.jakobmeier.ch/api/color/0/4").Result;
 
+            if (response.IsSuccessStatusCode)
+            {
+                string content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine("Success" + content);
+
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine(response.StatusCode);
+                Console.WriteLine($"The Error isch: {response.StatusCode}");
+            }
         }
     }
 }
